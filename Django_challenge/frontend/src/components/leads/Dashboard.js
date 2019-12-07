199cba7c935160115790c96002bac69123710fbd
TheGloryHouse <57041452+TheGloryHouse@users.noreply.github.com>
2019-12-07 13:49:47 +0200

    comit
    Commit
    
    Co-Authored-By: sams439 <sams439@users.noreply.github.com>

---
+++ Django_challenge/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+import Form from "./Form";
+
+
+
+
+export default function Dashboard() {
+  return (
+    <Fragment>
+      <Form />    
+    </Fragment>
+  );
+}
---
+++ Django_challenge/frontend/src/components/leads/Form.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { addLead } from "../../actions/leads";
+
+export class Form extends Component {
+  state = {
+    name: "",
+  };
+
+  static propTypes = {
+    addLead: PropTypes.func.isRequired
+  };
+
+  onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+  onSubmit = e => {
+    e.preventDefault();
+    const { name } = this.state;
+    const lead = { name };
+    this.props.addLead(lead);
+    this.setState({
+      name: "",
+    });
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <div className="card card-body mt-4 mb-4">
+        <h2>Search_for_users_follow</h2>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Name</label>
+            <input
+              className="form-control"
+              type="text"
+              name="Name"
+              onChange={this.onChange}
+              value={name}
+            />
+          </div>
+
+          <div className="form-group">
+            <button type="submit" className="btn btn-primary">
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  { addLead }
+)(Form);
